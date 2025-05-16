d90b3c1cac14d51eb81b63a704b7b45e652fca3f
KEmelyanov <KEmelyanov@nota.tech>
2025-05-16 17:17:52 +0300

    add files

--- src/test/java/com/example/junit4/StepTest.java
+++ src/test/java/com/example/junit4/StepTest.java
@@ -14,7 +14,7 @@ import java.util.stream.IntStream;
 public class StepTest {
 
     private static final String GLOBAL_PARAMETER = "global value";
-    private static final int LARGE_FILE_SIZE_MB = 10;
+    private static final int LARGE_FILE_SIZE_MB = 100;
     private static final int NUM_FILES_PER_TEST = 30;
 
     @Test
